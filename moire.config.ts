9956678d78e2f757cf0dcbc5b83bc15eaf3b71bf
Stone <anigiscur@gmail.com>
2026-02-13 21:25:59 -0600

    Update theme and UTC offset in configuration

--- moire.config.ts
+++ moire.config.ts
@@ -1,9 +1,9 @@
 export const config = {
   title: "Moire",
   author: "Aeris",
-  theme: "classic",
+  theme: "receipt",
   pageSize: 20,
-  utcOffset: 8,
+  utcOffset: -6,
   description: "Sync your thoughts from Apple Notes by Shortcuts.",
   keywords: "memo, thought, sync, apple notes, shortcuts, ios",
   url: "https://aeriszhu.com/moire"
